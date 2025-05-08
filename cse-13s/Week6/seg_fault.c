6666485690ad2f8e0153b1a76753dd452d95a873
David Wallace <dajwalla@ucsc.edu>
2025-05-08 12:17:46 -0700

    segfault program fixed

---
+++ cse-13s/Week6/seg_fault.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node{
+	char *data;
+	struct node *next;
+} node;
+
+node *new_node(char *data){
+	node *out = calloc(1, sizeof(node));
+	out->data = strdup(data);
+	out->next = NULL;
+	return out;
+}
+
+node *add_node(char *data, node *in){
+	node *new = new_node(data);
+	new->next = in;
+	return new;
+	
+}
+
+void free_ll(node *ll){
+
+	if(ll){
+		free(ll->data);
+		free_ll(ll->next);
+		free(ll);
+
+	}
+}
+
+int main(void){
+
+	int i = 10;
+
+	int *p = &i;
+
+	node *ll;
+	ll = new_node("Hello");
+	ll = add_node("There", ll);
+	ll = add_node("There", ll);
+
+	free_ll(ll);
+	
+	return 0;
+}
